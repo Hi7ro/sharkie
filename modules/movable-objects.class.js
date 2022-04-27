80c9a1230dc8c263a223b40a4e060e54939dc1cc
xv7ro <xv7ro@xyz.local>
2022-04-27 18:46:05 +0200

    added classes error free

---
+++ modules/movable-objects.class.js
@@ -0,0 +1,13 @@
+class MovableObject {
+  x = 100;
+  y = 600; 
+  img;
+
+  moveLeft(){
+    console.log('moving to the left');
+  }
+
+  moveRight(){
+    console.log('moving to the right');
+  }
+}
---
+++ modules/world.class.js
@@ -0,0 +1,8 @@
+class World {
+character = new Character();
+enemies = [
+    new PufferFish(),
+    new PufferFish(),
+    new PufferFish(),
+  ];
+}
